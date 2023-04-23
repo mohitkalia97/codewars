39fedc04a46622bf3eca72b58df862eb66f4d44b
mohitkalia97 <kalia.mohit1997@gmail.com>
2023-04-23 03:26:26 +0200

    FindTheParityOutlier - Task & Tests Done using Streams

--- src/main/java/findtheparityoutlier/FindTheParityOutlier.java
+++ src/main/java/findtheparityoutlier/FindTheParityOutlier.java
@@ -1,9 +1,10 @@
 package findtheparityoutlier;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FindTheParityOutlier {
     public static int findOutlier(int[] integers){
-        ArrayList<Integer> usedOdds = new ArrayList<>();
+       /* ArrayList<Integer> usedOdds = new ArrayList<>();
         ArrayList<Integer> usedEvens = new ArrayList<>();
 
         for (int integer : integers) {
@@ -17,6 +18,8 @@ public class FindTheParityOutlier {
             return usedEvens.get(0);
         } else {
             return usedOdds.get(0);
-        }
+        }*/
+        int[] arrayEven = Arrays.stream(integers).filter(i -> i % 2 == 0).toArray();
+        return (arrayEven.length == 1) ? arrayEven[0] : Arrays.stream(integers).filter(i -> i % 2 != 0).findAny().getAsInt();
     }
 }
--- src/test/java/findtheparityoutliertest/FindTheParityOutlierTest.java
+++ src/test/java/findtheparityoutliertest/FindTheParityOutlierTest.java
@@ -6,17 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindTheParityOutlierTest {
-   /* @ParameterizedTest
-    @CsvSource({
-            "1,1,1,2,1,1,2",
-            "2,2,2,1,2,2,2",
-            "2,6,8,-10,3,3",
-            "206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781,206847684",
-            "Integer.MAX_VALUE, 0, 1, 0"
-    })
-    public void testFindOutlier(int[] integers, int expectedOutput){
-        assertEquals(expectedOutput, integers);
-    }*/
    @Test
    public void testFindOutlier() {
        int[] arr1 = {2, 4, 6, 8, 10, 3, 12};
