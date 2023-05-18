eca895828d475f60558020fb49d84b81f409ba61
mohitkalia97 <102523042+mohitkalia97@users.noreply.github.com>
2023-05-19 01:39:22 +0200

    StreamsExercise - getAverageAge : Done

--- src/main/java/my_own_exercises/StreamExercises.java
+++ src/main/java/my_own_exercises/StreamExercises.java
@@ -17,6 +17,22 @@ public class StreamExercises {
         return strings.stream().map(String::toUpperCase).toList();
     }
 
+    public static double getAverageAge(String name){
+        Map<String, List<Integer>> personMap = new HashMap<>();
+
+        personMap.put("Alice", new ArrayList<>(List.of(25, 35, 20)));
+        personMap.put("Bob", new ArrayList<>(List.of(30)));
+        personMap.put("Charlie", new ArrayList<>(List.of(40)));
+
+        return personMap.entrySet().stream()
+                .filter(person -> person.getKey().equals(name))
+                .flatMapToInt(stringListEntry -> stringListEntry.getValue()
+                        .stream()
+                        .mapToInt(Integer::intValue))
+                .average()
+                .getAsDouble();
+    }
+
 
 
 
@@ -24,7 +40,7 @@ public class StreamExercises {
     public static void main(String[] args) {
         //System.out.println(getPositivePowersOfTen()); //done
         //System.out.println(toUppercase()); //done
-
+        System.out.println(getAverageAge("Alice"));
     }
 
 }
